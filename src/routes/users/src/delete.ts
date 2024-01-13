e1ccc7c3e404ea7e073a14fded5673303b61bc68
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-01-13 13:10:42 +0100

    fix: replace string by number for id type

--- src/routes/users/src/delete.ts
+++ src/routes/users/src/delete.ts
@@ -1,7 +1,7 @@
 import { Rest } from '../../../../utils.ts'
 
 export function erase(endpoint: string, apiKey: string) {
-	return (id: string) => {
+	return (id: number) => {
 		const requestURL = `${endpoint}/users/${id}.json`
 
 		return Rest.delete(requestURL, apiKey)
--- src/routes/users/src/update.ts
+++ src/routes/users/src/update.ts
@@ -2,7 +2,7 @@ import { Rest } from '../../../../utils.ts'
 import { User } from './create.ts'
 
 export function update(endpoint: string, apiKey: string) {
-	return (id: string, options: UpdateOptions) => {
+	return (id: number, options: UpdateOptions) => {
 		const requestURL = `${endpoint}/users/${id}.json`
 
 		return Rest.put(requestURL, apiKey, JSON.stringify(options))
